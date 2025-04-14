c818d1ece4424cc4c0b3e80d691a3d4c66ff48d0
paufrancisco <francisco.paulo.pf@gmail.com>
2025-04-15 01:50:23 +0800

    321

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask,render_template,request
-
+from flask import Flask,render_template,request,jsonify
+ 
+from flask_cors import CORS
 app = Flask(__name__)
 
 
-
+CORS(app)
 @app.route('/convert', methods=['POST'])
 def convert():
     # Static questions to return
